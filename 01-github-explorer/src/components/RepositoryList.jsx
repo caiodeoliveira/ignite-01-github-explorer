61efb2669dd21709338eb8d6317efc6cccd08288
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2021-11-16 22:39:38 -0300

    🚧 addeded an request to Github API

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,23 +1,29 @@
+import { useEffect, useState } from "react";
 import RepositoryItem from "./RepositoryItem";
 
 import "../styles/repositories.scss";
 
-const repository = {
-  name: "unform2",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform",
-};
+// https://api.github.com/orgs/rocketseat/repos
 
 function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, [repositories]);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
